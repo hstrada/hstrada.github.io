25148db1ed50d32e293f03da7171e5ebe9b9eb12
hstrada <h.strada@hotmail.com>
2021-03-13 15:23:00 -0300

    config: change import config

--- hstrada/src/pages/index.tsx
+++ hstrada/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/pages/Home.module.css';
+import {Profile} from '../components/Profile'
 import { Menu } from '../components/Menu';
-import { SectionLabel } from '../components/SectionLabel';
 
 export default function Home() {
   const changeMe = () => {
@@ -19,25 +19,7 @@ export default function Home() {
       </header>
 
       <main>
-        <section className={styles.profile}>
-          <SectionLabel number="01." text="Olá, eu sou a" />
-          <h1 className={styles.title}>Helena Strada</h1>
-          <p className={styles.description}>
-            e atuo como engenheira de software e sou apaixonada por livros e
-            tecnologia - principalmente, os dois juntos. Principais interesses
-            em arquitetura de software, código limpo e sempre estou buscando
-            algo para aprimorar e/ou aprender.
-          </p>
-          <p className={styles.highlight}>
-            Acredito que a <span>dedicação</span> seja transformação.
-          </p>
-          <div className={styles.icons}>
-            <img src="/assets/icons/github.svg" />
-            <img src="/assets/icons/arrow.svg" />
-            <img src="/assets/icons/in.svg" />
-            <img src="/assets/icons/arrow.svg" />
-          </div>
-        </section>
+        <Profile />
       </main>
 
       <footer>
