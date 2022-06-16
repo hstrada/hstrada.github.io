c6543a881da3ccc23b6b3cf1b4cb25f3c81e1a40
hstrada <6079947+hstrada@users.noreply.github.com>
2022-06-16 15:50:19 -0300

    refactoring project

--- hstrada/tailwind.config.js
+++ hstrada/tailwind.config.js
@@ -1,27 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
-  content: ['./src/**/*.{tsx,jsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
-    colors: {
-      blue: '#1fb6ff',
-      pink: '#ff49db',
-      orange: '#ff7849',
-      green: '#13ce66',
-      yellow: '#FFCC00',
-      gray: {
-        300: '#cccccc',
-        400: '#aaaaaa',
-        700: '#707070',
-        900: '#262626'
-      }
-    },
-    extend: {
-      fontFamily: {
-        sora: ['Sora', ...defaultTheme.fontFamily.sans]
-      }
-    }
+    extend: {}
   },
   plugins: []
 };
