6e40ba58f43d062360509d5dde13f75bfa9af1c2
hstrada <6079947+hstrada@users.noreply.github.com>
2022-06-16 16:09:06 -0300

    deleting unused css and adding font-family

--- hstrada/tailwind.config.js
+++ hstrada/tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    fontFamily: {
+      display: ['Epilogue']
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Sora', ...defaultTheme.fontFamily.sans]
+      }
+    }
   },
   plugins: []
 };
