b71677035fb83f2bb11c1486d6c81909da5653d7
hstrada <6079947+hstrada@users.noreply.github.com>
2022-06-18 20:31:12 -0300

    educational

--- hstrada/components/organisms/Educational/index.tsx
+++ hstrada/components/organisms/Educational/index.tsx
@@ -1,13 +1,23 @@
+import { FaInfo } from 'react-icons/fa'
 import { t } from '../../../core/utils/translate'
 import { EducationalItem } from '../../molecules/EducationalItem'
+import styles from './styles.module.css'
 
 export const Educational = () => {
   const out = Array.from(Array(3), (_, x) => x)
   return (
     <section className="px-6 mt-28">
       <div className="max-w-screen-lg flex flex-col mx-auto">
-        <h2 className="text-5xl leading-tight font-bold max-w-screen-lg font-sans">
-          {t('educational.title')}
+        <h2 className="text-5xl flex flex-row leading-tight font-bold max-w-screen-lg font-sans">
+          {t('educational.title')}{' '}
+          <span className={styles.tooltip}>
+            <FaInfo color="#F5F5F5" />
+            <span className={styles.text}>
+              <p className="font-display font-light text-gray-500 text-xs">
+                {t('educational.tooltip')}
+              </p>
+            </span>
+          </span>
         </h2>
         <div className="pt-16 grid grid-cols-12 gap-12">
           {out.map((index) => (
