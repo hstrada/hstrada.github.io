084f8ef7561a54b710d01a85961750b0395cf8b6
hstrada <6079947+hstrada@users.noreply.github.com>
2022-06-19 14:20:29 -0300

    handle select skill

--- hstrada/src/components/molecules/OpenSourceItem/index.tsx
+++ hstrada/src/components/molecules/OpenSourceItem/index.tsx
@@ -11,7 +11,7 @@ export const OpenSourceItem = ({
 }: TOpenSourceItemProps): JSX.Element => {
   return (
     <div>
-      <p className="font-display pb-4 h-32 md:h-20 font-light text-gray-700 text-sm">
+      <p className="font-display pb-4 h-12 sm:h-24 md:h-24 font-light text-gray-700 text-sm">
         {description}
       </p>
 
--- hstrada/src/components/organisms/Skills/index.tsx
+++ hstrada/src/components/organisms/Skills/index.tsx
@@ -4,49 +4,68 @@ import { SectionDiv } from '../../atoms/SectionDiv'
 import styles from './styles.module.css'
 
 const backend = [
-  'Java, Spring Boot, Spring MVC, Spring Security, Hibernate;',
-  'C#, .NET Core, EF Core;',
-  'API, REST, GraphQL, CQRS;',
-  'SQL Server, MySQL, MongoDB.',
+  '- Java, Spring Boot, Spring MVC, Spring Security, Hibernate;',
+  '- C#, .NET Core, EF Core;',
+  '- API, REST, GraphQL, CQRS;',
+  '- Clean Code, Clean Architecture, Design Patterns;',
+  '- SQL Server, MySQL, Redis.',
 ]
 
 const frontend = [
-  'HTML5, CSS3',
-  'ReactJs / React Native',
-  'Global State (Redux / Context)',
-  'Styled-Components',
-  'ES6',
-  'Testes (Jest / Enzyme / React Testing Library)',
-  'Typescript',
+  '- HTML5, CSS3;',
+  '- ReactJs / React Native;',
+  '- Global State (Redux / Context);',
+  '- Styled-Components;',
+  '- ES6;',
+  '- Testes (Jest / Enzyme / React Testing Library);',
+  '- Typescript.',
 ]
 
+const tools = [
+  '- Git + GitHub;',
+  '- GitHub Packages;',
+  '- CI/CD (CircleCI, GitHub Actions).',
+]
+
+const buildSkills = (skills: string[]) =>
+  skills.map((value) => `<div>${value}</div>`).join('')
+
 const skills = {
-  backend: backend.map((value) => `<div>${value}</div>`).join(''),
-  frontend: frontend.map((value) => `<div>${value}</div>`).join(''),
+  backend: buildSkills(backend),
+  frontend: buildSkills(frontend),
+  tools: buildSkills(tools),
 }
 
-export const Skills = () => {
-  const liStyle =
-    styles.skills +
-    ' bg-gray-300 rounded w-3/4 text-center p-2 text-lg font-sans hover:bg-slate-200 duration-300'
+const listStyle =
+  styles.skills +
+  ' bg-gray-300 rounded w-3/4 text-center p-2 text-lg font-sans hover:bg-slate-200 duration-300'
 
+export const Skills = () => {
   const [details, setDetails] = useState(skills['backend'])
+  const [selectedSkill, setSelectedSkill] = useState('backend')
+
+  const liStyle = (skill: string) => {
+    if (skill === selectedSkill) return listStyle.concat(' font-bold')
+    return listStyle
+  }
 
   const handleSelectSkill = (event) => {
-    setDetails(skills[event.target.accessKey])
+    const skill = event.target.accessKey
+    setDetails(skills[skill])
+    setSelectedSkill(skill)
   }
 
   return (
     <section className="px-6 mt-28">
       <SectionDiv title={t('skills.title')}>
         <div className="grid grid-cols-12 gap-4 pt-16">
-          <div className="col-span-3">
+          <div className="col-span-5 sm:col-span-3">
             <ul className="list-none space-y-4">
               <li
                 accessKey="backend"
                 key="backend"
                 onClick={handleSelectSkill}
-                className={liStyle}
+                className={liStyle('backend')}
               >
                 BackEnd
               </li>
@@ -54,9 +73,17 @@ export const Skills = () => {
                 accessKey="frontend"
                 key="frontend"
                 onClick={handleSelectSkill}
-                className={liStyle}
+                className={liStyle('frontend')}
               >
                 FrontEnd
+              </li>
+              <li
+                accessKey="tools"
+                key="tools"
+                onClick={handleSelectSkill}
+                className={liStyle('tools')}
+              >
+                Tools
               </li>
             </ul>
           </div>
