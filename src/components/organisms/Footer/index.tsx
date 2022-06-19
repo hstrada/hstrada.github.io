13fa7e8f13f2b5789598a4a039fb999a5c2695dd
hstrada <6079947+hstrada@users.noreply.github.com>
2022-06-19 15:34:21 -0300

    adding github pages

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,11 +1,16 @@
 import { SocialMedia } from 'components/molecules/SocialMedia'
 import Image from 'next/image'
 
+const myLoader = ({ src, width, quality }) => {
+  return `https://hstrada.github.io/public/${src}?w=${width}&q=${quality || 75}`
+}
+
 export const Footer = () => {
   return (
     <footer className="px-6 pb-6 pt-16 flex flex-row items-end justify-between">
       <div>
         <Image
+          loader={myLoader}
           src="/images/hs.png"
           alt="Helena Strada Profile Logo"
           width={23}
--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -3,11 +3,16 @@ import Image from 'next/image'
 import { FaGlobeAmericas, FaBars } from 'react-icons/fa'
 import { useTranslation } from '../../../core/hooks/useTranslate'
 
+const myLoader = ({ src, width, quality }) => {
+  return `https://hstrada.github.io/public/${src}?w=${width}&q=${quality || 75}`
+}
+
 export const Header = () => {
   const { changeCountry } = useTranslation()
   return (
     <header className="px-6 pt-6 flex flex-row justify-between">
       <Image
+        loader={myLoader}
         src="/images/hs.png"
         alt="Helena Strada Profile Logo"
         width={23}
