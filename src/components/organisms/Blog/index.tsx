260e432614299ccec4fdbf07bccc445e41d9f7e2
hstrada <6079947+hstrada@users.noreply.github.com>
2023-03-10 13:39:13 -0300

    chore: adding links to blog

--- src/components/organisms/Blog/index.tsx
+++ src/components/organisms/Blog/index.tsx
@@ -5,22 +5,27 @@ const posts = [
   {
     title: 'Criando workflows reutilizáveis com GitHub Actions',
     year: 2022,
+    url: 'https://balta.io/blog/criando-workflows-reutilizaveis-github-actions'
   },
   {
     title: 'Mantendo suas dependências atualizadas com o Dependabot',
     year: 2022,
+    url: 'https://hstrada.medium.com/mantendo-suas-dependências-atualizadas-com-o-dependabot-ac6003c25881'
   },
   {
     title: 'Alterando a cor do tema no React Native',
     year: 2021,
+    url: 'https://balta.io/blog/react-native-color-scheme'
   },
   {
     title: 'Escolhendo o simulador iOS',
     year: 2020,
+    url: 'https://medium.com/creditas-tech/escolhendo-o-simulador-ios-cddf9ded03bb'
   },
   {
-    title: 'Resume',
-    year: 2020,
+    title: 'Desmistificando o JOIN',
+    year: 2019,
+    url: 'https://hstrada.medium.com/desmistificando-o-join-becaba5ad0a6'
   },
 ]
 
@@ -32,7 +37,7 @@ export const Blog = () => {
           {posts.map((index) => (
             <div key={index.title} className="relative flex py-2 items-center">
               <span className="flex-shrink truncate mr-12 font-display font-light leading-6 text-gray-700 text-sm">
-                {index.title}
+                <a href={index.url}>{index.title}</a>
               </span>
               <div className="flex-grow border-b pt-2 border-dotted border-gray-400"></div>
               <span className="flex-shrink ml-12 font-display font-light leading-6 text-gray-700 text-sm">
